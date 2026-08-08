5dfb07874b1c3fa1d134fde1d6c90f05c00a7fa1
DecodingIntuition <decodingintuition@gmail.com>
2026-08-08 02:11:42 -0700

    fix: migrate Tree-sitter to main

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,52 @@
+local parsers = {
+	"astro",
+	"bash",
+	"c",
+	"cpp",
+	"css",
+	"diff",
+	"html",
+	"java",
+	"javascript",
+	"json",
+	"lua",
+	"luadoc",
+	"markdown",
+	"markdown_inline",
+	"python",
+	"query",
+	"regex",
+	"rust",
+	"sql",
+	"toml",
+	"tsx",
+	"typescript",
+	"vim",
+	"vimdoc",
+	"yaml",
+}
+
 return {
 	"nvim-treesitter/nvim-treesitter",
-	commit = "b065b591",
+	branch = "main",
+	lazy = false,
 	build = ":TSUpdate",
-	cmd = { "TSInstall", "TSInstallInfo", "TSUpdate", "TSUninstall" },
-	event = { "BufReadPost", "BufNewFile" },
-	main = "nvim-treesitter.configs",
-	opts = {
-		ensure_installed = {
-			"astro", "bash", "c", "cpp", "css", "diff", "html",
-			"java", "javascript", "json", "lua", "luadoc",
-			"markdown", "markdown_inline", "python", "query",
-			"regex", "rust", "sql", "toml", "tsx", "typescript",
-			"vim", "vimdoc", "yaml",
-		},
-		highlight = { enable = true },
-		indent = { enable = true, disable = { "c", "cpp" } },
-	},
+	config = function()
+		require("nvim-treesitter").install(parsers)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("user_treesitter", { clear = true }),
+			callback = function(event)
+				local lang = vim.treesitter.language.get_lang(vim.bo[event.buf].filetype)
+				if not lang or not vim.list_contains(parsers, lang) then
+					return
+				end
+
+				local started = pcall(vim.treesitter.start, event.buf, lang)
+				if started and lang ~= "c" and lang ~= "cpp" then
+					vim.bo[event.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
+			end,
+		})
+	end,
 }
