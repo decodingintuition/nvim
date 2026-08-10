157abade7a9434237c7524ea0a81c3bff280eb91
DecodingIntuition <decodingintuition@gmail.com>
2026-08-10 15:52:21 -0700

    fix: define tmux navigation for every input mode

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,16 +1,26 @@
+local directions = {
+	["<C-h>"] = { command = "TmuxNavigateLeft", desc = "Go to left pane" },
+	["<C-j>"] = { command = "TmuxNavigateDown", desc = "Go to lower pane" },
+	["<C-k>"] = { command = "TmuxNavigateUp", desc = "Go to upper pane" },
+	["<C-l>"] = { command = "TmuxNavigateRight", desc = "Go to right pane" },
+}
+
 return {
 	"christoomey/vim-tmux-navigator",
 	lazy = false,
+	init = function()
+		vim.g.tmux_navigator_no_mappings = 1
+	end,
 	config = function()
-		local directions = {
-			["<C-h>"] = "TmuxNavigateLeft",
-			["<C-j>"] = "TmuxNavigateDown",
-			["<C-k>"] = "TmuxNavigateUp",
-			["<C-l>"] = "TmuxNavigateRight",
-		}
-
-		for key, command in pairs(directions) do
-			vim.keymap.set("i", key, "<Cmd>" .. command .. "<CR>", { silent = true })
+		for key, mapping in pairs(directions) do
+			vim.keymap.set({ "n", "i", "t" }, key, function()
+				vim.cmd(mapping.command)
+			end, { silent = true, desc = mapping.desc })
 		end
+
+		vim.keymap.set("n", "<C-\\>", "<Cmd>TmuxNavigatePrevious<CR>", {
+			silent = true,
+			desc = "Go to previous pane",
+		})
 	end,
 }
