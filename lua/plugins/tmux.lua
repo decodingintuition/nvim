cf271fb8de8b505259b7f8e79a357b63c397a4ff
DecodingIntuition <decodingintuition@gmail.com>
2026-08-10 15:47:11 -0700

    fix: navigate tmux panes from insert mode

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,4 +1,16 @@
 return {
 	"christoomey/vim-tmux-navigator",
 	lazy = false,
+	config = function()
+		local directions = {
+			["<C-h>"] = "TmuxNavigateLeft",
+			["<C-j>"] = "TmuxNavigateDown",
+			["<C-k>"] = "TmuxNavigateUp",
+			["<C-l>"] = "TmuxNavigateRight",
+		}
+
+		for key, command in pairs(directions) do
+			vim.keymap.set("i", key, "<Cmd>" .. command .. "<CR>", { silent = true })
+		end
+	end,
 }
